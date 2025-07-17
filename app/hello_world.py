54f50715a47ad1ae3ba4d7bd5421f97ee1d4a854
Almog Devops <almog.levinshtein@gmail.com>
2025-07-17 06:43:44 +0300

    edit my application to working with flask

--- app/hello_world.py
+++ app/hello_world.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
-print("Hello from CI/CD!")
-print("By Almog Levinshtein")
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+VERSION = os.getenv("VERSION", "dev")
+
+@app.route("/")
+def hello():
+    return f"Hello from CI/CD! Version: {VERSION}\nBy Almog Levinshtein"
+
+if __name__ == "__main__":
+    print(f"Hello from CI/CD! Version: {VERSION}")
+    print("By Almog Levinshtein")
+    app.run(host="0.0.0.0", port=80)
